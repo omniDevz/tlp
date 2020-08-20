a6889f891cc3c169f3bc435aa11a58f0a14fe290
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-08-19 22:19:55 -0300

    init project

---
+++ src/assets/styles/default.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+:root {
+  font-size: 60%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #root {
+  min-height: 100vh;
+}
+
+body {
+  background: ${(props) => props.theme.colors.secondary};
+  color: ${(props) => props.theme.colors.tertiary};
+  font-family: 'Roboto', sans-serif;
+  line-height: 1.2;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+  margin: 0;
+}
+
+a {
+  color: ${(props) => props.theme.colors.tertiary};
+  text-decoration: none;
+  font-size: 1.8rem;
+  cursor: pointer;
+}
+
+li {
+  list-style: none;
+}
+
+button {
+  border: none;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+}
+
+button:focus {
+  outline: none;
+}
+
+div, section, article, footer, #root {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+`;

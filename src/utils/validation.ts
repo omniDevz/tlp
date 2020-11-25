956fe40b564c5641cb04afcf4df15141ae37306c
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-11-25 15:01:29 -0300

    change validation in dateMinToDay

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,6 @@
 const validation = {
   dateMinToDay(value: string) {
-    return new Date(value).valueOf() < Date.now();
+    return new Date(value) < new Date();
   },
   email(email: string) {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
